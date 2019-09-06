11b080c85737cf8dc735e3e51e7ee2dc521ef9f3
mor bargig <morbargig@gmail.com>
2019-09-06 20:30:52 +0300

    first uploade

--- src/config/route.js
+++ src/config/route.js
@@ -1,5 +1,5 @@
-const local ='http://localhost:8000/'
-// const heroku = '/'
-const route  = local
+// const local ='http://localhost:8000/'
+const heroku = '/'
+const route  = heroku
 
 export default route
