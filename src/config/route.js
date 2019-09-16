9db716d59fa527928372a73fb9fcc44913810526
mor bargig <morbargig@gmail.com>
2019-09-16 04:14:49 +0300

    more clear to Admin

--- src/config/route.js
+++ src/config/route.js
@@ -1,18 +1,4 @@
-
-// if ( )
-
-const aws = require('aws-sdk');
-
-let s3 = new aws.S3({
-  accessKeyId: process.env.S3_KEY,
-  secretAccessKey: process.env.S3_SECRET
-});
-
-console.log(s3)
-// const routee = require("../../server")
-// console.log(routee)
-// if (process.env.PORT !== undefined) {
-    const route = '/'
-// } else { const route = 'http://localhost:8000/' }
+const route = '/'
+// const route = 'http://localhost:8000/'
 
 export default route
