05255d6480334e4325c53760c411d3b540d62b70
mor bargig <morbargig@gmail.com>
2019-09-16 04:10:22 +0300

    more clear to Admin

--- src/config/route.js
+++ src/config/route.js
@@ -1,7 +1,16 @@
 
 // if ( )
-const routee = require("../../server")
-console.log(routee)
+
+const aws = require('aws-sdk');
+
+let s3 = new aws.S3({
+  accessKeyId: process.env.S3_KEY,
+  secretAccessKey: process.env.S3_SECRET
+});
+
+console.log(s3)
+// const routee = require("../../server")
+// console.log(routee)
 // if (process.env.PORT !== undefined) {
     const route = '/'
 // } else { const route = 'http://localhost:8000/' }
